22e4e68d6e3d0ea7ba3b1beaece36009e9c15e01
icefirewater <50715640+icefirewater@users.noreply.github.com>
2021-03-15 20:37:36 +0530

    Add files via upload
    Scene Transition - 4 scenes are there : Master, Scene1, Scene2, Scene3 . Master scene is having three buttons and on clicking on individual buttons transition takes place to respective scenes

---
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnButtonClick()
+    {
+        Debug.Log("Button clicked");
+        SceneManager.LoadScene(newScene);
+    }    
+}
